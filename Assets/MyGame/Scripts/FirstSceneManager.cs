a88dc449abe2e10482d0e2ee97304b9fb37e386b
angelinahansich <angelina.hanisch04@htl-salzburg.ac.at>
2023-01-30 17:49:57 +0100

    added FirstSceneManager

---
+++ Assets/MyGame/Scripts/FirstSceneManager.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstSceneManager : MonoBehaviour
+{
+
+    public void PressStart()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+}
